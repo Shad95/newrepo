eff09445c58db662f03666b134c3e554ddde9e18
Shad Khan <shadk1952@gmail.com>
2024-11-19 19:14:41 +0530

    second version added for test

--- JustTesting.java
+++ JustTesting.java
@@ -3,5 +3,6 @@ package com.example.practice;
 public class JustTesting {
     public static void main(String[] args) {
         System.out.println("this is just for testing purposes");
+        System.out.println("this is v2 change");
     }
 }
